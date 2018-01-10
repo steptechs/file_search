4b5f399981cf1baed6ea9d4b4b0963fb7c84b31e
steptechs <35179431+steptechs@users.noreply.github.com>
2018-01-10 21:41:41 +0000

    Finished tutorial

--- program.py
+++ program.py
@@ -1,4 +1,8 @@
 import os
+import collections
+
+SearchResult = collections.namedtuple("SearchResult",
+                                      "file, line, text")
 
 
 def main():
@@ -13,7 +17,13 @@ def main():
         print("We can't search for nothing!")
         return
 
-    search_folders(folder, text)
+    matches = search_folders(folder, text)
+    for m in matches:
+        print("------- MATCH -------")
+        print("file: " + m.file)
+        print("line: {}".format(m.line))
+        print("match: " + m.text.strip())
+        print()
 
 
 def print_header():
@@ -35,11 +45,40 @@ def get_folder_from_user():
 
 def get_search_text_from_user():
     text = input("What are you searching for [single phrases only]? ")
-    return text
+    return text.lower()
 
 
 def search_folders(folder, text):
-    print("Would search {} for {}".format(folder, text))
+    # all_matches = []
+    items = os.listdir(folder)
+
+    for item in items:
+        full_item = os.path.join(folder, item)
+        if os.path.isdir(full_item):
+            # matches = search_folders(full_item, text)
+            # all_matches.extend(matches)  # add all items from collection individually
+            # for m in matches:
+            #    yield m
+            yield from search_folders(full_item, text)
+        else:
+            # matches = search_file(full_item, text)
+            # all_matches.extend(matches)
+            # for m in matches:
+            #    yield m
+            yield from search_file(full_item, text)
+
+    # return all_matches
+
+
+def search_file(filename, search_text):
+    with open(filename, "r", encoding="utf-8") as fin:
+
+        line_num = 0
+        for line in fin:
+            line_num += 1
+            if line.lower().find(search_text) >= 0:
+                m = SearchResult(line=line_num, file=filename, text=line)
+                yield m
 
 
 if __name__ == '__main__':
